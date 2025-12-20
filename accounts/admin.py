246916447b8205cf948e036fd1969b6f1c26af4f
Edward Solon <eddie@edwardsolon.com>
2025-12-20 15:26:32 -0600

    Is_PetAgency flag

--- accounts/admin.py
+++ accounts/admin.py
@@ -3,10 +3,11 @@ from .models import CustomUser,Profile
 
 # Register your models here.
 class CustomUserModelAdmin(admin.ModelAdmin):
-        list_display = ('username', 'email', 'id') # Fields to display in the list view
+        list_display = ('username', 'email','is_agency_dir','is_agency_vol','id') # Fields to display in the list view
         search_fields = ('username', 'email') # Fields to search by
         fields = ('email','username', ('first_name','last_name'),
-        ('password'),('hint'),('is_superuser','is_staff','is_active'),('created_at','last_login'),
+        ('password'),('hint'),('is_superuser','is_staff','is_active'),
+        ('is_agency_dir','is_agency_vol'),('created_at','last_login'),
         ('groups','user_permissions'))
 
         # ... other customizations
